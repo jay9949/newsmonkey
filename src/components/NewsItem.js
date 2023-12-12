325b5b67cf315bb0bfcfe5b0e9c16b6d012096d9
jay9949 <jay37192@gmail.com>
2023-12-12 15:29:20 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card shadow mb-5 rounded border-primary">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger"> {source} </span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title} </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            style={{ marginLeft: "17rem", marginTop: "1rem" }}
+            className="btn btn-sm btn-primary "
+          >
+            Read More &rarr;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
